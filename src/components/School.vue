7251b12d930ae31bee069fa9bd5580499185c1ff
Mr.yang <2372818565@qq.com>
2022-07-05 15:35:41 +0800

    新增 xlsx filesaver   导出xlsx文件

--- src/components/School.vue
+++ src/components/School.vue
@@ -6,7 +6,7 @@
 
     </div>
     <div>
-      <el-table :data="tableData" size="small">
+      <el-table :data="tableData" size="small" id="Table">
         <el-table-column prop="bookname" label="书名">
         </el-table-column>
         <el-table-column prop="bookms" label="描述">
@@ -30,6 +30,7 @@
       <div class="felx">
         <div>
           <el-button size="mini" @click="openDialog">添加</el-button>
+          <el-button size="mini" @click="exportXlsx">导出</el-button>
         </div>
         <div class="Pages">
           <el-pagination @current-change="handleCurrentChange" :current-page="pagenation.current"
@@ -68,6 +69,11 @@
   import Poz from '../utils/httpClient.js'
   // const Poz = require('../utils/httpClient.js')
   // const axios = require('axios')
+
+  //导入filesaver 把xlsx文件下载到地址
+  import FileSaver from 'file-saver'
+  //导入xlsx
+  const XLSX = require('xlsx')
   export default {
     created() {
       console.log("页面刷新了");
@@ -100,6 +106,36 @@
       openDialog() {
         this.dialogVisible = true
 
+      },
+      exportXlsx() {
+        console.log("导出", XLSX, FileSaver)
+        // 使用 this.$nextTick 是在dom元素都渲染完成之后再执行
+        this.$nextTick(function () {
+          // 设置导出的内容是否只做解析，不进行格式转换 false：要解析， true:不解析
+          const xlsxParam = {
+            raw: true
+          }
+          const wb = XLSX.utils.table_to_book(document.querySelector('#Table'), xlsxParam)
+          // 导出excel文件名
+          let fileName = '书籍' + new Date().getTime() + '.xlsx'
+
+          const wbout = XLSX.write(wb, {
+            bookType: 'xlsx',
+            bookSST: true,
+            type: 'array'
+          })
+          try {
+            // 下载保存文件
+            FileSaver.saveAs(new Blob([wbout], {
+              type: 'application/octet-stream'
+            }), fileName)
+          } catch (e) {
+            if (typeof console !== 'undefined') {
+              console.log(e, wbout)
+            }
+          }
+          return wbout
+        })
       },
       resetForm(formName) {
         console.log("重置");
